482ca3d37f65c32ca9ca850d11d86cceee9cd0f5
tiagocb <tiagoboz@hotmail.com>
2013-08-12 23:26:10 -0300

    Adicionado a estrutura do dicionario

---
+++ include/comp_dict.h
@@ -0,0 +1,41 @@
+/*
+  comp_dict.h
+
+  Contém estrutura de um dicionário implementado como uma tabela hash, e operações:
+  - Criar dicionário
+  - Procurar por elemento dada uma chave
+  - Inserir um elemento e a sua chave associada
+  - Atualizar um elemento dada a sua chave
+  - Deletar um elemento dada a sua chave
+  - Obter o número de elementos armazenados
+  - Remover todo o conteúdo do dicionário
+  - Destruir o dicionário
+  - Imprimir o dicionário
+*/
+
+typedef struct{
+    char *key;
+    int value;
+} comp_dict_item_t;
+
+typedef struct _comp_dict_node_t{
+    comp_dict_item_t *item;
+    struct _comp_dict_node_t *next;
+} comp_dict_node_t;
+
+typedef struct{
+    int numberOfLists;
+    int numberOfElements;
+    comp_dict_node_t **table;
+} comp_dict_t;
+
+int createDictionaty(comp_dict_t *dict, int numberOfLists);
+unsigned int hashFunction(int numberOfLists, char *key);
+comp_dict_item_t *searchKey(comp_dict_t dict, char *key);
+int insertKey(comp_dict_t *dict, char *key, int value);
+int updateKey(comp_dict_t dict, char *key, int newValue);
+int deleteKey(comp_dict_t *dict, char *key);
+int getNumberOfKeys(comp_dict_t dict);
+void clearDictionaryContent(comp_dict_t *dict);
+void destroyDictionary(comp_dict_t *dict);
+void printDictionary(comp_dict_t dict);
--- src/main.c
+++ src/main.c
@@ -17,13 +17,13 @@ void linkedListTest(){
 	comp_list_t *list;
 	createList(&list);
 	printList(list);
-	
+
 	//insert 20 randon values and print it
 	printf("\nInserting 20 randon values...\n");
 	int i;
 	for(i = 0; i < 20; i++) insertTail(&list, rand()%100);
 	printList(list);
-	
+
 	//check 1st, 2nd and 3rd values and print list
 	printf("\nChecking and deleting 1st, 2nd and 3rd values...\n");
 	int value = getFirst(list);
@@ -54,6 +54,79 @@ void linkedListTest(){
 	printList(list);
 }
 
+void dictionaryTest(){
+    //creates dictionary with 20 lists and print it
+    printf("\nCreating dictionary with 20 lists...\n");
+    comp_dict_t dictionary;
+    createDictionaty(&dictionary, 5);
+    printDictionary(dictionary);
+
+    //insert keys and print dictionary
+    printf("\n");
+    int quit = 0;
+    char input[256];
+    while(!quit){
+        printf("Insert a key: ");
+        gets(input);
+        if(strcmp(input, "quit") == 0){
+            quit = 1;
+            break;
+        }
+        insertKey(&dictionary, input, rand()%100);
+    }
+    printDictionary(dictionary);
+
+    //search for keys and print dictionary
+    printf("\nSearch for a key: ");
+    comp_dict_item_t *item;
+    quit = 0;
+    while(!quit){
+        gets(input);
+        if(strcmp(input, "quit") == 0){
+            quit = 1;
+            break;
+        }
+        item = searchKey(dictionary, input);
+        if(item != NULL) printf("%s -> %d\n", item->key, item->value);
+        else printf("not found\n");
+    }
+    printDictionary(dictionary);
+
+    //update values and print dictionary
+    printf("\nUpdate a key: ");
+    quit = 0;
+    while(!quit){
+        gets(input);
+        if(strcmp(input, "quit") == 0){
+            quit = 1;
+            break;
+        }
+        if(updateKey(dictionary, input, 999) != 0) printf("not found\n");
+    }
+    printDictionary(dictionary);
+
+    //delete keys and print dictionary
+    printf("\nDelete a key: ");
+    quit = 0;
+    while(!quit){
+        gets(input);
+        if(strcmp(input, "quit") == 0){
+            quit = 1;
+            break;
+        }
+        if(deleteKey(&dictionary, input) != 0) printf("not found\n");
+    }
+    printDictionary(dictionary);
+
+    //clear content, print it, destroy it and print it again
+    printf("\nRemoving content...\n");
+    clearDictionaryContent(&dictionary);
+    printDictionary(dictionary);
+    printf("\nDestroying dictionary...\n");
+    destroyDictionary(&dictionary);
+    printDictionary(dictionary);
+}
+
 int main (int argc, char **argv)
 {
 	int token = TOKEN_ERRO;
@@ -62,6 +135,7 @@ int main (int argc, char **argv)
 	//}
 
 	linkedListTest();
+	dictionaryTest();
 
 	return 0;
 }
